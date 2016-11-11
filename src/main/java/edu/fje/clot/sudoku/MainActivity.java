7d0e4ecf30fc86fab2090736ac01d98e3b5b8cb5
MetalMor <srgark@gmail.com>
2016-11-11 18:38:27 +0100

    20161111: clase cargadora eliminada - loader no funciona * nombre del label de color de fondo

--- src/main/java/edu/fje/clot/sudoku/MainActivity.java
+++ src/main/java/edu/fje/clot/sudoku/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.fje.clot.sudoku;
 
+import android.support.v4.content.ContentResolverCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import edu.fje.clot.sudoku.action.LoadGameAction;
@@ -12,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
 
     public MainActivity() {
         btnPlay = (Button) findViewById(R.id.play);
-        btnPlay.setOnClickListener(new LoadGameAction());
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_game);
+            }
+        });
     }
 
     @Override
